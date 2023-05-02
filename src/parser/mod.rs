7536cfa190f5a93edf11ecb3b5d13a73b2e1c0a2
Nathan Kamenchu <kamenchunathan@gmail.com>
2023-05-02 19:59:16 +0300

    feat: Add parsing for a simple function

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,9 +1,14 @@
 #![allow(unused)]
 
+mod combinators;
+
 use nom::{
     branch::alt,
     bytes::complete::take_while1,
     character::complete::{alpha1, char},
+    combinator::opt,
+    multi::separated_list0,
+    sequence::delimited,
     IResult, Parser,
 };
 use nom_supreme::parser_ext::ParserExt;
@@ -12,32 +17,75 @@ use nom_supreme::{
     tag::complete::tag,
 };
 
+use crate::parser::combinators::optional_space_delim;
+
 #[derive(Debug)]
-pub enum Definition {
+pub enum Definition<'a> {
     TypeDefinition,
-    FunctionDefinition,
+    FunctionDefinition {
+        ident: &'a str,
+        parameters: Vec<&'a str>,
+        body: Expression,
+    },
 }
 
 #[derive(Debug)]
-pub enum Expression {}
+pub enum Expression {
+    Unit,
+}
 
 fn is_valid_ident_char(inp: char) -> bool {
     inp.is_alphanumeric() || inp == '_' || inp == '\''
 }
 
-fn ident(input: &str) -> IResult<&str, &str, ErrorTree<&str>> {
-    // A valid identifier starts with an underscore or letter
+/// Parses a valid identifer in the meri language
+///
+/// A valid identifier starts with an underscore or letter
+/// and contains alphabetic characters, numbers, underscores
+/// or the apostrophe
+fn parse_ident(input: &str) -> IResult<&str, &str, ErrorTree<&str>> {
     alt((alpha1, tag("_")))
         .context("First Character of an an identifier")
         .parse(input)?;
-
     let (input, ident) = take_while1(is_valid_ident_char)(input)?;
 
     Ok((input, ident))
 }
 
-fn function(input: &str) -> IResult<&str, &str> {
-    todo!()
+/// Parses an expression
+fn expression(input: &str) -> IResult<&str, Expression, ErrorTree<&str>> {
+    opt(optional_space_delim(tag("void")))(input).map(|(inp, res)| {
+        (
+            inp,
+            res.map(|_| Expression::Unit).unwrap_or(Expression::Unit),
+        )
+    })
+}
+
+fn function_body(input: &str) -> IResult<&str, Expression, ErrorTree<&str>> {
+    delimited(tag("{"), expression, tag("}"))(input)
+}
+
+fn parse_function(input: &str) -> IResult<&str, Definition, ErrorTree<&str>> {
+    let (input, ident) = parse_ident(input)?;
+    let (input, _) = optional_space_delim(tag("="))(input)?;
+    let (input, parameters) = delimited(
+        tag("("),
+        // TODO: add support for annotating types of parameters
+        separated_list0(tag(","), optional_space_delim(parse_ident)),
+        tag(")"),
+    )(input)?;
+    let (input, _) = optional_space_delim(tag("=>"))(input)?;
+    let (input, body) = optional_space_delim(function_body)(input)?;
+
+    Ok((
+        input,
+        Definition::FunctionDefinition {
+            ident,
+            parameters,
+            body,
+        },
+    ))
 }
 
 #[cfg(test)]
@@ -52,12 +100,31 @@ mod tests {
 
     #[test]
     fn ident_parses() {
-        assert_matches!(ident("wow"), Ok(("", "wow")));
-        assert_matches!(ident("_ping"), Ok(("", "_ping")));
-        assert_matches!(ident("_ping'"), Ok(("", "_ping'")));
+        assert_matches!(parse_ident("wow"), Ok(("", "wow")));
+        assert_matches!(parse_ident("_ping"), Ok(("", "_ping")));
+        assert_matches!(parse_ident("_ping'"), Ok(("", "_ping'")));
         assert_matches!(
-            ident("3ping"),
+            parse_ident("3ping"),
             Err(nom::Err::Error(GenericErrorTree::Stack { .. }))
         );
+    }
+
+    #[test]
+    fn fn_no_params_and_empty_body() {
+        let src = "f = () => {}";
+        assert_eq!(
+            assert_matches!(
+                parse_function(src),
+                Ok((
+                    "",
+                    Definition::FunctionDefinition {
+                        ident: "f",
+                        parameters,
+                        body: Expression::Unit
+                    }
+                )) => parameters
+            ),
+            Vec::<&str>::new()
+        )
     }
 }
